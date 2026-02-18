e57a421cd7ea43caa83f6451b8c95a61b65af2e7
purav s <alwayspurav@gmail.com>
2026-02-18 21:40:05 +0530

    quotations update

--- src/components/QuoteSection.tsx
+++ src/components/QuoteSection.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useRef } from "react";
 import TypewriterText, { useInView } from "./TypewriterText";
 
 interface QuoteSectionProps {
@@ -16,10 +15,9 @@ export default function QuoteSection({ quote }: QuoteSectionProps) {
     <div className="quote-section" ref={ref as React.RefObject<HTMLDivElement>}>
       <div className="quote-inner">
         <span className="quote-mark">&ldquo;</span>
-        <blockquote className="quote-text">
-          <TypewriterText text={quote} speed={40} trigger={inView} />
-        </blockquote>
-        <span className="quote-dot">.</span>
+        <p className="quote-text">
+          <TypewriterText text={quote.endsWith(".") ? quote : quote + "."} speed={40} trigger={inView} />
+        </p>
       </div>
     </div>
   );
